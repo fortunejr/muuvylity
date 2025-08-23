26278fdc17c8ade6fabcdad79d6f2c155bc72e16
ikennaex <ikennaexcel@gmail.com>
2025-08-23 15:34:51 +0100

    responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
   return (
     <footer className="bg-stone-950">
       <div className="container mx-auto px-4 sm:px-6 lg:px-10 py-10">
-        <div className="flex flex-col lg:flex-row justify-between gap-10">
+        <div className="flex flex-col lg:flex-row lg:flex-wrap justify-between gap-10">
           {/* Logo and Description */}
-          <div className="lg:w-1/3">
+          <div className="lg:w-1/4">
             <Link to={"/"}>
               <img className="h-14 mb-4" src={logo} alt="LumiGrid logo" />
             </Link>
@@ -22,7 +22,7 @@ const Footer = () => {
           </div>
 
           {/* Contact Info */}
-          <div className="lg:w-1/3">
+          <div className="lg:w-1/4">
             <h2 className="font-bold text-white pb-3 pt-7 lg:pt-0">
               Contact Information
             </h2>
@@ -32,7 +32,8 @@ const Footer = () => {
             <p className="text-white text-sm mt-2">📧 info@omphoenix.com.ng</p>
           </div>
 
-          <div className="w-full h-96 rounded-lg overflow-hidden shadow-md">
+          {/* Map */}
+          <div className="lg:w-1/4 w-full h-64 lg:h-48 rounded-lg overflow-hidden shadow-md">
             <iframe
               src="https://www.google.com/maps/embed?pb=!4v1755958888439!6m8!1m7!1sGxBfeXwJPVOXsq1t1D71Ng!2m2!1d5.567936549623464!2d5.79700192865511!3f208.2!4f0!5f0.7820865974627469"
               width="100%"
@@ -45,30 +46,16 @@ const Footer = () => {
           </div>
 
           {/* Social Icons */}
-          <div className="lg:w-1/3 pt-7 lg:pt-0">
+          <div className="lg:w-1/4 pt-7 lg:pt-0">
             <h2 className="font-bold text-white pb-3">Follow Us</h2>
             <div className="flex items-center gap-4 text-white">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href=""
-              >
+              <a target="_blank" rel="noopener noreferrer" href="">
                 <FaFacebook size={20} />
               </a>
-
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href=""
-              >
+              <a target="_blank" rel="noopener noreferrer" href="">
                 <FaInstagram size={20} />
               </a>
-
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href=""
-              >
+              <a target="_blank" rel="noopener noreferrer" href="">
                 <FaLinkedinIn size={20} />
               </a>
             </div>
