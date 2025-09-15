33ec52a5e7d449fe52b7f9d369a37db688db00f4
Fortune <fortuneokechukwu206@gmail.com>
2025-09-15 14:44:37 +0100

    changes

--- src/Pages/MeetTheTeam/MeetTheTeam.jsx
+++ src/Pages/MeetTheTeam/MeetTheTeam.jsx
@@ -3,6 +3,13 @@ import { FaUserTie } from "react-icons/fa";
 
 const teamMembers = [
   {
+    role: "Chairman",
+    name: "Ahmed C.Egbenumolise",
+    bio: `Mr. Egbenumolise has his Bachelors degree in Public Administration  from Ambroise Alli University,  Ekpoma, Edo State and Master's Degree in Strategy and Security Administration (MASSA) from Nigeria Defense Academy, NDA, Kaduna. Ahmed is a security consultant,  sport developer and a passionate entrepreneur with flourishing interests in construction. Private security services,  communication and entertainment 
+
+He sits on the Board of several enterprises.  He is a sport lover, avid reader and prolific writer`,
+  },
+  {
     role: "Management Consultant",
     name: "Chidi Ndego",
     bio: `Chidi earned a first class degree in Petroleum Engineering from the University of Benin and has since built a distinguished career as a Data and AI professional. With over five years of experience spanning data analytics, data science, engineering, and artificial intelligence, he has made significant contributions across industries.
@@ -10,21 +17,21 @@ const teamMembers = [
 Chidi has delivered impactful results in roles at Standard Bank Group, General Electric, and The Weather Company, combining technical expertise with strong leadership to drive innovation and create value.`,
   },
   {
-    role: "Management Consultant",
-    name: "Idiebise Oghenevwogaga Nelson (MNSE, CWI, MNIMechE, MNIPES, MIAENG)",
+    role: "Director",
+    name: "Engr Nelson Idiebise",
     bio: `He attended University of Benin and attained a Bachelor Degree in Mechanical Engineering.
     
 A registered Engineer (Coren No. 48,367) with over a decade of onshore/offshore experiences in engineering construction, project management, fabrication, installation, welding inspection, HSE and QA/QC. Proficient in HSE compliance to ensure a safe working environment, including risk assessments, surveys, machinery upgrades and maintenance. Passionate about providing technical support to improve productivity and increase efficiency to achieve and exceed operational objectives.`,
   },
   {
-    role: "Chief Finance Officer",
+    role: "Director/ CFO",
     name: "Ifeoma Onokpasa",
     bio: `Ifeoma Onokpasa is a seasoned finance professional with extensive expertise in strategy, treasury management, and corporate finance. She holds a Bachelor’s degree in Economics from Covenant University, Ota, an MBA from Lagos Business School, Pan-Atlantic University, Lagos, and a Finance Certificate from Yale University, USA.
 
 Her career includes notable experience at Financial Derivatives Company, where she served in various capacities, and at Futureview Financial Services, where she led the Treasury and Corporate Finance functions. Ifeoma is also an accomplished administrator with a strong passion for strategic planning and financial management.`,
   },
   {
-    role: "",
+    role: "Director",
     name: "Ms Sandra Hitler",
     bio: `Sandra is safety and environmental enthusiast. She has a diploma in Civil Engineering and a degree in Microbiology.
 
@@ -32,18 +39,12 @@ Her career includes notable experience at Financial Derivatives Company, where s
   },
   {
     role: "Management Consultant",
-    name: "Kelly Ochuko Egode",
+    name: "Engr. Kelly Ochuko Egode",
     bio: `Kelly Ochuko Egode is a seasoned Electrical/Electronics Engineer and multidisciplinary technology leader with over 18 years of experience across energy, finance, agriculture, infrastructure, and industrial automation. As a COREN/NSE-certified professional, he combines hands-on engineering expertise with cutting-edge AI and IoT solutions to drive efficiency and innovation in the oil & gas sector.
 
 A graduate of the Federal University of Technology Akure (B.Eng, Electrical/Electronics) and holder of an MSc in AI & Data Science (Distinction) University of Hull, UK. Kelly bridges traditional engineering with Industry 4.0 technologies. His work has been recognized by Innovate UK, and the Nigerian Society of Engineers.`,
   },
-  {
-    role: "Chairman",
-    name: "Ahmed C.Egbenumolise (BPA,CP,MASSA)",
-    bio: `Mr.Egbenumolise has his Bachelors degree in Public Administration  from Ambroise Alli University,  Ekpoma, Edo State and Master's Degree in Strategy and Security Administration (MASSA) from Nigeria Defense Academy, NDA, Kaduna. Ahmed is a security consultant,  sport developer and a passionate entrepreneur with flourishing interests in construction. Private security services,  communication and entertainment 
-
-He sits on the Board of several enterprises.  He is a sport lover, avid reader and prolific writer`,
-  }
+  
 ];
 
 const MeetTheTeam = () => {
