70f69b58b4d9df0130f8d118da2532e1189f707b
Fortune <fortuneokechukwu206@gmail.com>
2025-09-30 10:46:06 +0100

    cta btns

--- src/Components/Herosection/Herosection.jsx
+++ src/Components/Herosection/Herosection.jsx
@@ -22,9 +22,7 @@ const buttonVariants = {
 
 const Herosection = () => {
   return (
-    <section
-      className="relative overflow-hidden bg-cover bg-center bg-no-repeat"
-    >
+    <section className="relative overflow-hidden bg-cover bg-center bg-no-repeat">
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat kenburns-top pointer-events-none z-0"
         style={{ backgroundImage: `url(${heroimg})` }}
@@ -42,47 +40,49 @@ const Herosection = () => {
         >
           <p className="text-customYellow font-bold">Muuvylity Logistics Ltd</p>
           <h1 className="text-3xl lg:text-5xl font-black text-white tracking-normal pb-6">
-            Delivering reliable haulage and supply chain solutions across Nigeria and beyond.
+            Delivering reliable haulage and supply chain solutions across
+            Nigeria and beyond.
           </h1>
           <p className="text-white text-base lg:text-lg opacity-90">
-            The aim is to provide
-            comprehensive, reliable, and effcient logistics solutions,
-            optimizing supply chains and building strong client
-            partnerships.
+            The aim is to provide comprehensive, reliable, and effcient
+            logistics solutions, optimizing supply chains and building strong
+            client partnerships.
           </p>
 
           {/* CTA Buttons */}
-          {/* <div className="pt-9 flex flex-row justify-center lg:justify-start items-center gap-4 flex-wrap">
+          <div className="pt-9 flex flex-col lg:flex-row justify-center items-center gap-4">
             <motion.a
               target="_blank"
-              href="https://wa.me/2348033670069"
+              href="https://wa.me/2349091112210"
               rel="noreferrer"
               variants={buttonVariants}
               custom={0}
               initial="hidden"
               whileInView="visible"
               viewport={{ once: true }}
+              className="w-[70%] lg:w-auto"
             >
-              <button className="flex gap-2 items-center bg-white px-4 py-3 lg:px-8 lg:py-4 font-bold text-black cursor-pointer hover:scale-105 transition text-sm lg:text-base">
-                <FaWhatsapp size={22} className="lg:size-27" />
+              <button className="w-full lg:w-auto rounded-full flex gap-2 items-center justify-center bg-white px-6 py-4 lg:px-8 lg:py-4 font-bold text-black cursor-pointer hover:scale-105 transition text-base lg:text-base">
+                <FaWhatsapp size={24} className="lg:size-27" />
                 Chat us
               </button>
             </motion.a>
 
             <motion.a
-              href="mailto:Omphoenixlimited@gmail.com"
+              href="mailto:info@muuvilitylogisticsltd.com"
               variants={buttonVariants}
               custom={1}
               initial="hidden"
               whileInView="visible"
               viewport={{ once: true }}
+              className="w-[70%] lg:w-auto"
             >
-              <button className="flex gap-2 items-center border-white hover:bg-lime-400 border-2 px-4 py-3 lg:px-8 lg:py-4 font-bold text-white cursor-pointer hover:scale-105 transition text-sm lg:text-base">
-                <MdOutlineMail size={22} className="lg:size-27" />
+              <button className="w-full lg:w-auto rounded-full flex gap-2 items-center justify-center border-white hover:bg-customYellow border-2 px-6 py-4 lg:px-8 lg:py-4 font-bold text-white cursor-pointer hover:scale-105 transition text-base lg:text-base">
+                <MdOutlineMail size={24} className="lg:size-27" />
                 Email us
               </button>
             </motion.a>
-          </div> */}
+          </div>
         </motion.div>
       </div>
     </section>
